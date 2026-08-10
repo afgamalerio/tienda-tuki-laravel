089ab53803aa8015a23b362108cfbfbcd3f15e3e
afgamalerio <anahifernandezg@gmail.com>
2026-08-09 21:09:19 -0300

    Se completa el CRUD de productos y categorias.

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -2,10 +2,82 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use App\Models\Producto;
+
 class ProductoController extends Controller
 {
     public function index()
     {
-        return response()->json([]);
+        $productos = Producto::all();
+
+        return response()->json([
+            'mensaje' => 'Listado de productos',
+            'productos' => $productos
+        ]);
+    }
+
+    public function show(int $id)
+    {
+        $producto = Producto::find($id);
+
+        if (!$producto) {
+            return response()->json([
+                'mensaje' => 'Producto no encontrado'
+            ], 404);
+        }
+
+        return response()->json($producto);
+    }
+
+    public function store(Request $request)
+    {
+        $producto = new Producto();
+
+        $producto->nombre = $request->nombre;
+        $producto->descripcion = $request->descripcion;
+        $producto->imagen = $request->imagen;
+        $producto->precio = $request->precio;
+        $producto->stock = $request->stock;
+        $producto->color = $request->color;
+        $producto->categoria_id = $request->categoria_id;
+
+        $producto->save();
+
+        return response()->json($producto, 201);
+    }
+
+    public function update(Request $request, int $id)
+    {
+        $producto = Producto::find($id);
+
+        if (!$producto) {
+            return response()->json(['mensaje' => 'Producto no encontrado'], 404);
+        }
+
+        $producto->nombre = $request->nombre;
+        $producto->descripcion = $request->descripcion;
+        $producto->imagen = $request->imagen;
+        $producto->precio = $request->precio;
+        $producto->stock = $request->stock;
+        $producto->color = $request->color;
+        $producto->categoria_id = $request->categoria_id;
+
+        $producto->save();
+
+        return response()->json($producto);
+    }
+
+    public function destroy(int $id)
+    {
+        $producto = Producto::find($id);
+
+        if (!$producto) {
+            return response()->json(['mensaje' => 'Producto no encontrado'], 404);
+        }
+
+        $producto->delete();
+
+        return response()->json(['mensaje' => 'Producto eliminado correctamente']);
     }
 }
